8e27316dcfbc499e0dcf5c21975c5eb9a1b80c36
Nayem783982 <majedulislam223311@gmail.com>
2026-03-15 22:25:07 +0600

    implement authenticeiton

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,16 @@ import express, { Request, Response } from "express"
 import { MealRouter } from "./modules/meal/meal.route";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.APP_URL || "http://localhost:3000",
+    credentials: true
+}));
 app.use(express.json())
+
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.use("/meal", MealRouter)
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import { Role } from "../constants/role";
+import { Status } from "../constants/status";
 
 
 export const auth = betterAuth({
@@ -10,4 +12,19 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+    user: {
+        additionalFields: {
+            role: {
+                type: "string",
+                defaultValue: Role.USER,
+                required:false
+            },
+            status: {
+                type: "string",
+                defaultValue: Status.ACTIVE,
+                required:false
+            }
+        }
+    },
+    trustedOrigins: [process.env.APP_URL || "http://localhost:3000"],
 });
