e359eaa02eada8e3f643e8d0ce9f88429cf4bdcf
Nayem783982 <majedulislam223311@gmail.com>
2026-03-15 17:16:24 +0600

    fixed the auth.ts file error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { Prisma } from "../../generated/prisma/client";
+import { prisma } from "./prisma";
 
 
 export const auth = betterAuth({
-    database: prismaAdapter(Prisma, {
+    database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
     emailAndPassword: {
